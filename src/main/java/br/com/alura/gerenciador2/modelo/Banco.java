02fab2e3ae354f938b7fc05333a25f22893a4176
carlos5veiga <carlos5veiga@gmail.com>
2022-08-18 11:23:31 -0300

    Login e senha

--- src/main/java/br/com/alura/gerenciador2/modelo/Banco.java
+++ src/main/java/br/com/alura/gerenciador2/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
@@ -18,6 +19,17 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		Banco.lista.add(empresa);
 		Banco.lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u2.setLogin("ana");
+		u2.setSenha("12345");
+		
+		Banco.listaUsuarios.add(u1);
+		Banco.listaUsuarios.add(u2);
+		
 	}
 	
 	public void adiciona(Empresa empresa) {
@@ -50,6 +62,16 @@ public class Banco {
 			if (empresa.getId() == id) {
 				System.out.println(empresa.getNome());
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		
+		for(Usuario usuario: listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
