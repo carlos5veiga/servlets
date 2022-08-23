62c11456171459af541d38e47e7f2bb2ce3360a5
carlos5veiga <carlos5veiga@gmail.com>
2022-08-23 08:28:55 -0300

    filters init and destroy

--- src/main/java/br/com/alura/gerenciador2/servlet/AutorizacaoFilter.java
+++ src/main/java/br/com/alura/gerenciador2/servlet/AutorizacaoFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +14,15 @@ import javax.servlet.http.HttpSession;
 
 //@WebFilter("/entrada")   //Configurando atraves do web.xml
 public class AutorizacaoFilter extends HttpFilter implements Filter {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {}
        
+	@Override
+	public void destroy() {}
+	
+	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
 		System.out.println("Autorizacao Filter");
--- src/main/java/br/com/alura/gerenciador2/servlet/ControladorFilter.java
+++ src/main/java/br/com/alura/gerenciador2/servlet/ControladorFilter.java
@@ -8,17 +8,23 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador2.acao.Acao;
 
 //@WebFilter("/entrada")   //Configurando atraves do web.xml
 public class ControladorFilter extends HttpFilter implements Filter {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {}
        
+	@Override
+	public void destroy() {}
+	
+	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
 		System.out.println("Controlador Filter");
